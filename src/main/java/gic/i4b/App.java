2aaaf44c4fcd6ce6d5521e0c7d9f887546a08658
SuosPhearith <suosphearith@gmail.com>
2024-05-10 10:00:42 +0700

    first commit

---
+++ src/main/java/gic/i4b/App.java
@@ -0,0 +1,13 @@
+package gic.i4b;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
